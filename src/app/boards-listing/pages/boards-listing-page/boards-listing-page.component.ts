c502019455372337ee9667c45abc1814cc5a50db
BeataMaro <beatamaro@interia.pl>
2023-05-21 21:37:13 +0200

    fix: remove commented code snippets

--- src/app/boards-listing/pages/boards-listing-page/boards-listing-page.component.ts
+++ src/app/boards-listing/pages/boards-listing-page/boards-listing-page.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Iboard } from '../../../shared/models/board.model';
+import { Router } from '@angular/router';
 import { select, Store } from '@ngrx/store';
-import { getBoards, deleteBoard } from '../../store/board/board-actions';
+import { Observable, shareReplay } from 'rxjs';
+
+import { Iboard } from '../../../shared/models/board.model';
+import { getBoards } from '../../store/board/board-actions';
 import { deleteColumn } from '../../store/column/column-actions';
 import {
   BoardIdSelector,
@@ -9,15 +12,13 @@ import {
   ErrorSelector,
   isLoadingSelector,
 } from '../../store/board/board-selectors';
-import { Observable, shareReplay } from 'rxjs';
 
-import { ConfirmationDialog } from 'src/app/core/components/confirmation-dialog/confirmation-dialog.component';
-import { MatDialog } from '@angular/material/dialog';
 import { ColumnIdSelector } from '../../store/column/column-selector';
 import { BoardsService } from '../../service/boards.service';
-import { Router } from '@angular/router';
-
-import { windowInnerWidth, handleSize } from 'src/app/shared/helpers/window-size';
+import {
+  windowInnerWidth,
+  handleSize,
+} from 'src/app/shared/helpers/window-size';
 
 @Component({
   selector: 'app-board',
@@ -36,10 +37,8 @@ export class BoardsListingPageComponent implements OnInit {
 
   constructor(
     private store: Store,
-    // private dialog: MatDialog,
     private BoardsService: BoardsService,
-    private router: Router,
-   
+    private router: Router
   ) {
     this.isLoading$ = this.store.pipe(select(isLoadingSelector));
     this.isError$ = this.store.pipe(select(ErrorSelector));
@@ -54,36 +53,18 @@ export class BoardsListingPageComponent implements OnInit {
     // this.isLoading$ = this.store.pipe(select(isLoadingSelector));
     if (!this.isLoggedIn) this.router.navigateByUrl('/home');
     this.allBoards$ = this.store.select(BoardsSelector) || [];
-    this.BoardsService.getAllBoards().pipe(shareReplay()).subscribe().unsubscribe();
+    this.BoardsService.getAllBoards()
+      .pipe(shareReplay())
+      .subscribe()
+      .unsubscribe();
     this.store.dispatch(getBoards());
   }
 
   handleSize(event: any) {
-    this.mybreakpoint = handleSize(event)
+    this.mybreakpoint = handleSize(event);
   }
-
-  // deleteBoard(board: { boardId: string }) {
-  //   this.store.dispatch(deleteBoard(board));
-  //   this.BoardsService.deleteBoard(board.boardId).subscribe();
-  //   this.ngOnInit();
-  // }
 
   deleteColumn(board: { boardId: string; columnId: string }) {
     this.store.dispatch(deleteColumn(board));
   }
-
-  // openDialog(board: { boardId: string }) {
-  //   const dialogRef = this.dialog.open(ConfirmationDialog, {
-  //     data: {
-  //       message: 'Do you want to delete the board and the associated tasks?',
-  //       cancelButtonText: 'Cancel',
-  //     },
-  //   });
-
-  //   dialogRef.afterClosed().subscribe((confirmed: boolean) => {
-  //     if (confirmed) {
-  //       this.deleteBoard(board);
-  //     }
-  //   });
-  // }
 }
