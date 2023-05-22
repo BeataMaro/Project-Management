11b0c4c28f19f98d2ab4862384801b67c2f76cb8
BeataMaro <beatamaro@interia.pl>
2023-05-22 23:02:53 +0200

    feat: implement resposive welcome page styles

--- src/app/welcome/pages/welcome-page/welcome-page.component.ts
+++ src/app/welcome/pages/welcome-page/welcome-page.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/user-login/service/auth.service';
+import {
+  windowInnerWidth,
+  handleSize,
+} from 'src/app/shared/helpers/window-size';
+import { Observable, map } from 'rxjs';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-welcome-page',
@@ -10,15 +16,24 @@ import { AuthService } from 'src/app/user-login/service/auth.service';
 export class WelcomePageComponent implements OnInit {
   isLoggedin = false;
   authToken = '';
+  mybreakpoint: number = 1;
+  colspan$: Observable<number>;
 
-  constructor(private router: Router, private auth: AuthService) {
+  constructor(private router: Router, private auth: AuthService, private breakpointObserver: BreakpointObserver) {
     this.isLoggedin = this.auth.isUserLoggedIn();
     this.authToken = this.auth.getToken();
+    this.colspan$ = breakpointObserver.observe('(max-width: 599.99px)')
+    .pipe(map(state => state.matches ? 1 : 2));
   }
 
   ngOnInit() {
+    this.mybreakpoint = windowInnerWidth();
     // console.log(`IsloggedIn = ${this.isLoggedin}`)
     // console.log(`authToken = ${this.authToken}`)
    if (this.authToken) this.router.navigateByUrl('/boards');
+  }
+
+  handleSize(event: any) {
+    this.mybreakpoint = handleSize(event);
   }
 }
